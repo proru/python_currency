f381e9eb054118c5ac970fdc15bff2113b3c33bd
Shavkunov Konstantin <purposef@yandex.ru>
2021-05-05 00:58:55 +0500

    fix(logging): add logging and try except. do not stop eventloop in exception

--- rest_framework.py
+++ rest_framework.py
@@ -15,11 +15,10 @@ import datetime
 import xml.etree.ElementTree as ET
 import sys
 import argparse
-from abc import ABC, abstractmethod
+
 import logging
+from typing import Any, AsyncIterator, Awaitable, Callable, Dict
 import traceback
-
-
 
 
 class RestFrame:
@@ -32,10 +31,6 @@ class RestFrame:
     logger = None
 
     def __init__(self, list_currency, script_args, logger):
-        # for item in list_currency:
-        #     if not item.startswith('--') or item.startswith('--help'):
-        #         list_currency.remove(item)
-        # list_currency.append('--help')
         self.currencies = list(filter(lambda x: x.startswith('--') and not x.startswith('--help'), list_currency))
         for key in self.currencies:
             self.currency_data[key.replace('-', '')] = {
@@ -51,7 +46,6 @@ class RestFrame:
         self.currencies = [key for key, value in self.currency_data.items()]
         self.currencies.remove('rub')
         self.app = self.init()
-
 
     @staticmethod
     def createParser(list_currency):
@@ -81,7 +75,7 @@ class RestFrame:
         combinations = list(itertools.combinations(self.currencies, r=2))
         for (keyf, keys) in combinations:
             txt = txt + ' / ' + keyf + '-' + keys + ' ' + \
-                  str(round(float(self.currency_data[keyf]['current_rate']) / float(self.currency_data[keys]['current_rate']), 2))
+                  str(round(float(self.currency_data[keyf]['current_rate']) / float(self.currency_data[keys]['current_rate']),2))
         txt = txt + '\nsum:'
         sum_rub = 0
         for key in self.currencies:
@@ -92,14 +86,18 @@ class RestFrame:
         for key in self.currencies:
             txt = txt + ' / ' + key + ' ' + str(round(sum_rub / float(self.currency_data[key]['current_rate']), 2))
         # txt = str([(key, value) for key, value in self.currency_data.items()])
+        self.logger.debug('end handle request /amount/get')
         return web.Response(text=txt, content_type='text/plain')
 
     async def amount_set(self, request):
-        data = await request.json()
-        for key, value in data.items():
-            self.currency_data[key]['prev_value'], self.currency_data[key]['current_value'] = \
-                self.currency_data[key]['current_value'], value
-        txt = str([(key, value) for key, value in self.currency_data.items()])
+        try:
+            data = await request.json()
+            for key, value in data.items():
+                self.currency_data[key]['prev_value'], self.currency_data[key]['current_value'] = \
+                    self.currency_data[key]['current_value'], value
+            txt = str([(key, value) for key, value in self.currency_data.items()])
+        except Exception as e:
+            self.logger.debug(request + 'handled request /amount/get')
         return web.Response(text=txt, content_type='text/plain')
 
     async def modify_currency(self, request):
@@ -108,10 +106,11 @@ class RestFrame:
             self.currency_data[key]['prev_value'], self.currency_data[key]['current_value'] = \
                 self.currency_data[key]['current_value'], value + self.currency_data[key]['current_value']
         txt = str([(key, value) for key, value in self.currency_data.items()])
+        self.logger.debug('handle request ' + str(request))
         return web.Response(text=txt, content_type='text/plain')
 
     async def set_currency(self, request):
-        name = request.match_info.get('name', "Anonymous")
+        name = request.match_info.get('name', 'usd')
         txt = "Hello, {}".format(name)
         data = await request.json()
         for key, value in data.items():
@@ -119,52 +118,71 @@ class RestFrame:
         return web.Response(text=txt)
 
     async def get_currency(self, request):
-
-        name = request.match_info.get('name', "Anonymous")
-        txt = "Hello, {} {}, {}".format(name, self.currency_data[name]['current_rate'], name)
+        name = request.match_info.get('name', 'usd')
+        txt = "Currency rate: {} {}".format(name, self.currency_data[name]['current_rate'])
         return web.Response(text=txt)
-        # return web.Response(text="<h1> Async Rest API using aiohttp : Health OK </h1>", content_type='text/html')
 
     async def control_data(self):
+        self.logger.info('Check changing data value or rates')
         print_now = False
         for key in self.currencies:
-            if self.currency_data[key]['current_rate'] != self.currency_data[key]['prev_rate'] or self.currency_data[key]['current_value'] != self.currency_data[key]['prev_value']:
+            if self.currency_data[key]['current_rate'] != self.currency_data[key]['prev_rate'] or \
+                    self.currency_data[key]['current_value'] != self.currency_data[key]['prev_value']:
                 self.currency_data[key]['prev_value'] = self.currency_data[key]['current_value']
                 print_now = True
         if print_now:
             response = await self.amount_get(request=None)
+            self.logger.debug(response.body.decode("utf-8"))
             print(response.body.decode("utf-8"))
 
     async def send_request_to_data(self, session):
+        self.logger.info('Update data about currencies rate')
         async with session.get(url="https://www.cbr-xml-daily.ru/daily_utf8.xml", ) as response:
             string_xml = await response.text()
-        # response = requests.get("https://www.cbr-xml-daily.ru/daily_utf8.xml")
-        # string_xml = response.content
         root = ET.fromstring(string_xml)
-        # print('USD', root.findall(".*[CharCode='USD']/Value")[0].text, 'EUR',
-        #       root.findall(".*[CharCode='EUR']/Value")[0].text)
         for key in self.currencies:
             self.currency_data[key]['prev_rate'], self.currency_data[key]['current_rate'] = \
                 self.currency_data[key]['current_rate'], root.findall(".*[CharCode='" + key.upper() + "']/Value")[0].text.replace(",", ".")
-        print('print get data', datetime.datetime.now())
 
     async def start_schedul(self):
         headers = {}
         async with aiohttp.ClientSession(headers=headers) as session:
-            await self.send_request_to_data(session)
-            while True:
+            try:
+                self.logger.info('Start receiving data about currency schedule')
                 await self.send_request_to_data(session)
+            except Exception as ex:
+                self.logger.error(traceback.format_exc())
+            while True:
+                try:
+                    await self.send_request_to_data(session)
+                except Exception as ex:
+                    self.logger.error(traceback.format_exc())
                 await asyncio.sleep(self.period - datetime.datetime.now().second % self.period)
 
     async def start_control_schedul(self):
+        self.logger.info('start control data on changing')
         await asyncio.sleep(10)
         while True:
             await asyncio.sleep(5 - datetime.datetime.now().second % 5)
-            await self.control_data()
+            try:
+                await self.control_data()
+            except Exception as ex:
+                self.logger.error(traceback.format_exc())
 
+    @web.middleware
+    async def error_middleware(self, request: web.Request, handler: Callable[[web.Request], Awaitable[web.StreamResponse]],) -> web.StreamResponse:
+        try:
+            self.logger.info(request)
+            return await handler(request)
+        except web.HTTPException:
+            self.logger.error(traceback.format_exc())
+        except asyncio.CancelledError:
+            self.logger.error(traceback.format_exc())
+        except Exception as ex:
+            self.logger.error(traceback.format_exc())
 
     def init(self):
-        app = web.Application()
+        app = web.Application(middlewares=[self.error_middleware])
         app.router.add_get("/", self.health)
         app.router.add_get("/amount/get", self.amount_get)
         app.router.add_post("/amount/set", self.amount_set)
@@ -179,20 +197,22 @@ class RestFrame:
         await runner.setup()
         site = web.TCPSite(runner, 'localhost', 8080)
         await site.start()
+        self.logger.info("======= Serving on http://127.0.0.1:8080/ ======")
         print("======= Serving on http://127.0.0.1:8080/ ======")
-        # pause here for very long time by serving HTTP requests and
-        # waiting for keyboard interruption
         await asyncio.Event().wait()
 
     async def main(self):
+        self.logger.info('Start event loop')
         await asyncio.gather(
             self.start_schedul(),
             self.start_server(),
             self.start_control_schedul(),
             # web._run_app(self.app, port=8000),
         )
+
+
 def configure_log(log_level='DEBUG'):
-    logfile = 'log_1.log'
+    logfile = './logs/log_'+str(datetime.date.today())+'.log'
     log = logging.getLogger("my_log")
     log.setLevel(logging.__dict__[log_level])
     FH = logging.FileHandler(logfile)
@@ -201,29 +221,9 @@ def configure_log(log_level='DEBUG'):
     log.addHandler(FH)
     return log
 
-# def error_log(logger, line_no):
-#     err_formater = logging.Formatter('%(asctime)s : [%(levelname)s][LINE ' + line_no + '] : %(message)s')
-#     FH.setFormatter(err_formater)
-# 	log.addHandler(FH)
-# 	## пишем сообщение error
-# 	log.error(traceback.format_exc())
-# 	## возвращаем базовый формат сообщений
-# 	FH.setFormatter(basic_formater)
-# 	log.addHandler(FH)
-
-# logging.basicConfig(
-#     level=logging.DEBUG,
-#     format='(%(threadName)-10s) %(message)s',
-# )
-# logger_format = '%(asctime)s:%(threadName)s:%(message)s'
-# logging.basicConfig(format=logger_format, level=logging.INFO, datefmt="%H:%M:%S")
 
 if __name__ == "__main__":
     logger = configure_log()
-    try:
-        raise Exception
-    except Exception as e:
-        logger.error(traceback.format_exc())
     list_currency = sys.argv[1:]
     parser = RestFrame.createParser(list_currency=list_currency)
     script_args = parser.parse_args(sys.argv[1:])
